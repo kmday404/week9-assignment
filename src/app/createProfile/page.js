51eda5aecb65e7a25d11e9a8ff06da0b3d5cbe4a
Katherine Day <kmday125@gmail.com>
2024-10-27 10:32:50 +0000

    form added to create profile

--- src/app/createProfile/page.js
+++ src/app/createProfile/page.js
@@ -8,7 +8,7 @@
 
 //currentUser is the current user logged in - you can import this from clerk???? ?
 
-import { currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 
 import pg from "pg";
 
@@ -17,17 +17,44 @@ export default async function CreateProfilePage({ params }) {
     connectionString: process.env.NEXT_PUBLIC_DATABASE_URL,
   });
 
-  await db.query(`INSERT INTO users (username, clerk_key) VALUES ($1, $2)`, [
-    currentUser.username,
-    currentUser.id,
-  ]);
-  // revalidatePath(`/`);
-  // redirect(`/`);
-  console.log(currentUser.id);
+  const { userId } = await auth();
+
+  async function handleSubmit(formValues) {
+    "use server";
+    formValues = {
+      username: formValues.get("username"),
+      bio: formValues.get("bio"),
+      interests: formValues.get("interests"),
+    };
+    console.log(formValues);
+
+    await db.query(
+      `INSERT INTO users (username, bio, interests, clerk_key) VALUES ($1, $2, $3, $4)`,
+      [formValues.username, formValues.bio, formValues.interests, userId]
+    );
+
+    revalidatePath(`/createProfile`);
+    redirect(`/user/${userId}`);
+  }
 
   return (
     <>
       <h1>create profile</h1>
+
+      <form action={handleSubmit} className="flex flex-col items-center p-10">
+        <label htmlFor="Username">My username:</label>
+        <input type="text" id="name" name="name" required />
+        <label htmlFor="bio">About me:</label>
+        <input type="text" id="bio" name="bio" required />
+        <label htmlFor="interests">Interests:</label>
+        <input type="text" id="interests" name="interests" required />
+        <button
+          type="submit"
+          className="border-green-400 border-4 bg-yellow-600 text-black-600 p-2 m-3"
+        >
+          Submit
+        </button>
+      </form>
     </>
   );
 }
